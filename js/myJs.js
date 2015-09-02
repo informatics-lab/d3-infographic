e946ea9b5e49b245e59aaa0ef5336144e1065ea3
Thomas Powell <t.powell.meto@gmail.com>
2015-09-02 11:32:01 +0100

    amend menu loaders

--- js/myJs.js
+++ js/myJs.js
@@ -153,19 +153,15 @@ var toggleMenu = function () {
     }
 };
 
-var fillIcon = function(id) {
-
-};
-
 var fillLine = function(id) {
-   d3.select(id).select(".fill")
+   d3.select(id).select(".progress-bar")
        .transition()
            .duration(1000)
-           .style("width", "100%")
+           .style("width", "100%");
 }
 
 var emptyLine = function(id) {
-   d3.select(id).select(".fill")
+   d3.select(id).select(".progress-bar")
        .transition()
            .duration(1000)
            .style("width", "0%")
